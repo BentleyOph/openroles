c76245ed6c3920ca8670c0429e80eda9267c49f3
bentley <bentenzz001@gmail.com>
2025-08-15 04:18:40 +0300

    feat: model relationships

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,14 @@
 class Job < ApplicationRecord
   belongs_to :company
+
+  searchkick
+
+  def searchdata
+    {
+      title: title,
+      description: description,
+      remote: remote,
+      company_name: company.name
+    }
+  end
 end
