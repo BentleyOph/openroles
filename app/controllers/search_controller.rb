d3dd800b15a52c894a742ecb18a504cb3163db04
bentley <bentenzz001@gmail.com>
2025-08-15 15:00:33 +0300

    feat: natural language search using searchkick's field boosting and typo tolerance

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,15 @@
 class SearchController < ApplicationController
   def index
+    if params[:query].present?
+      @jobs = Job.search(
+        params[:query],
+        includes: [ :company ],
+        operator: "or",
+        fields: [ "title^5", "company_name^3", "description" ],
+        misspellings: { below: 1 }
+      )
+    else
+      @jobs = []
+    end
   end
 end
